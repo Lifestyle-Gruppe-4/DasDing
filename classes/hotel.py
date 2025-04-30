6d4f6449adb6f222c7f511b75799b7b5f43ce5bc
Thomas <thomas.bartels@students.fhnw.ch>
2025-04-30 10:53:52 +0200

    Methoden ergänzt

--- classes/hotel.py
+++ classes/hotel.py
@@ -1,4 +1,6 @@
 from classes.address import Address
+from classes.room import Room
+from datetime import datetime
 
 class Hotel:
     def __init__(self, hotel_id:int, name:str, stars:int, address:Address):
@@ -6,9 +8,23 @@ class Hotel:
         self.name = name
         self.stars = stars
         self.address = address
+        self.rooms: list[Room] = []
 
-    def get_available_rooms(self):
-        pass
+    def add_room(self, room):
+        self.rooms.append(room)
 
-    def get_rooms_by_stars(self):
+    def delete_room(self, room):
         pass
+    # deletion process needs to be added
+
+    def get_available_rooms(self, start_date: datetime, end_date: datetime) -> list['Room']:
+        return [room for room in self.rooms if room.is_available(start_date, end_date)]
+
+    def get_rooms_by_stars(self, min_stars: int, max_stars: int) -> list['Room']:
+        if min_stars <= self.stars <= max_stars:
+            return self.rooms
+        else:
+            return []
+
+    def get_hotel_list(self):
+        return self.name, self.stars, self.address, self.rooms
