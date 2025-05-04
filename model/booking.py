cff43fe9e370930453b2d319feb36800e0373bd7
Thomas <thomas.bartels2@gmail.com>
2025-05-04 16:58:53 +0200

    Created the method. Added also new Method that are not in our Model.

--- model/booking.py
+++ model/booking.py
@@ -1,25 +1,63 @@
 from datetime import datetime
 from model.guest import Guest
 from model.room import Room
+from model.invoice import Invoice
 
 class Booking:
-    def __init__(self, booking_id:int, check_in_date:datetime, check_out_date:datetime, is_cancelled:bool, total_amount:float, guest:Guest, room:Room):
-        self.booking_id = booking_id
+    booking_counter = 1
+
+    def __init__(self, check_in_date: datetime, check_out_date: datetime, guest: Guest, room: Room):
+        self.booking_id = Booking.booking_counter
+        Booking.booking_counter += 1
+
         self.check_in_date = check_in_date
         self.check_out_date = check_out_date
-        self.is_cancelled = is_cancelled
-        self.total_amount = total_amount
         self.guest = guest
         self.room = room
+        self.is_cancelled = False
+        self.total_amount = self.calculate_total_amount()
+        self.invoice = None
 
-    def make_booking(self):
-        pass
+    def get_booking_details(self) -> str:
+        booking_status = "Cancelled" if self.is_cancelled else "Active"
+        return (f"Booking ID: {self.booking_id},  Guest: {self.guest.first_name} {self.guest.last_name} "
+                f"Room: {self.room.room_nr}, From: {self.check_in_date}, to: {self.check_out_date} "
+                f"Total: {self.total_amount} CHF, Status: {booking_status}")
+
+    def update_dates(self, new_check_in: datetime, new_check_out: datetime):
+        if new_check_out < new_check_in:
+            raise ValueError("Check-out must be the same or after check-in date!")
+        self.check_in_date = new_check_in
+        self.check_out_date = new_check_out
+        self.total_amount = self.calculate_total_amount()
+
+    def calculate_total_amount(self) -> float:
+        nights = (self.check_out_date - self.check_in_date).days
+        if nights <= 0:
+            raise ValueError("Booking must be at least 1 night.")
+        return nights * self.room.price_per_night
 
     def cancel_booking(self):
-        pass
+        self.is_cancelled = True
+        print(f"Booking ID: {self.booking_id} has been cancelled.")
 
-    def view_booking_history(self):
-        pass
+    def generate_invoice(self):
+        if not self.invoice:
+            self.invoice = Invoice(self.total_amount, self)
+        return self.invoice
 
-    def get_booking(self):
-        pass
+    def is_future_booking(self) -> bool:
+        return self.check_in_date > datetime.now()
+
+    def is_active_booking(self) -> bool:
+        return self.check_in_date <= datetime.now() <= self.check_out_date and not self.is_cancelled
+
+    def stay_duration(self) -> int:
+        return (self.check_out_date - self.check_in_date).days
+
+    def print_confirmation(self):
+        print("------Booking Confirmation------")
+        print(self.get_booking_details())
+        print(f"Duration: {self.stay_duration()} Nights")
+        print(f"Total: {self.total_amount} CHF")
+        print("--------------------------------")
--- model/invoice.py
+++ model/invoice.py
@@ -1,16 +1,56 @@
 from datetime import datetime
+from datetime import timedelta
 from model.booking import Booking
 
-
-
 class Invoice:
-    def __init__(self, invoice_id:int, issue_date:datetime, total_amount:float, booking:Booking, is_paid:bool):
-        self.invoice_id = invoice_id
-        self.issue_date = issue_date
-        self.total_amount = total_amount
+    invoice_counter = 1
+
+    def __init__(self, total_amount: float, booking: Booking):
+        self.invoice_id = Invoice.invoice_counter
+        Invoice.invoice_counter += 1
+
+        self.total_amount =  total_amount
+        self.issue_date = datetime.today()
+        self.is_paid = False
         self.booking = booking
-        self.is_paid = is_paid
 
     def mark_as_paid(self):
-        pass
+        self.is_paid = True
+        print(f"Invoice {self.invoice_id} is marked as paid.")
 
+    def get_invoice_details(self) -> str:
+        status = "Is Paid" if self.is_paid else "Payment Pending"
+        return (f"Invoice ID: {self.invoice_id}, Amount: {self.total_amount:.2f} CHF, "
+                f"Issue Date: {self.issue_date.isoformat()}, Status: {status}, Booking ID: {self.booking.booking_id}")
+
+    def send_invoice_by_email(self, email_address: str):
+        print(f"Invoice {self.invoice_id} is sent to email: {email_address}")
+
+    def is_overdue(self) -> bool:
+        due_date = self.issue_date + timedelta(days=14)
+        return not self.is_paid and datetime.today() > due_date
+
+    def apply_discount(self, percent: float):
+
+        if percent < 0 or percent > 100:
+            print("Invalid discount percent.")
+            return
+
+        if not self.is_paid:
+            discount = self.total_amount * (percent / 100)
+            self.total_amount -= discount
+            print(f"Discount of {percent}% applied. \nNew Total: {self.total_amount:.2f} CHF")
+        else:
+            print("Cannot apply discount. Invoice already paid.")
+
+    def get_invoice_summary(self) -> str:
+        status = "Is Paid" if self.is_paid else "Payment Pending"
+        return (f"[Invoice #{self.invoice_id}] Date: {self.issue_date} | Total: {self.total_amount:.2f} CHF | "
+                  f"Status: {status} | Booking ID: {self.booking.booking_id}")
+
+    def cancel_invoice(self):
+        if self.is_paid:
+            print("Invoice already paid. Cannot be cancelled")
+        else:
+            self.total_amount = 0
+            print(f"Invoice {self.invoice_id} has been cancelled.")
