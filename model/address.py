758a43b17ad808bebf9639d630b17c6a569abe44
michele <michele.lepori@bluewin.ch>
2025-05-04 13:23:50 +0200

    Hotel und Address erstellt

--- model/address.py
+++ model/address.py
@@ -32,6 +32,14 @@ class Address:
         return f"Address ID: {self.address_id}, Street: {self.street}, City: {self.city}, ZIP: {self.zip}"
 
 
+addr1 = Address(address_id=1,street="Hauptstrasse 5",city="Basel", zip="4051")
+addr2 = Address(address_id=2,street="Allmendweg 69",city="Sissach", zip="4450")
+addr3 = Address(address_id=3,street="Oberer Chriesmattweg 5",city="Böckten",zip="4461")
+
+print(addr1)
+print(addr2)
+print(addr3)
+
 
 #Address Class defined by Silian in a first step
 #class Address:
@@ -44,11 +52,3 @@ class Address:
     #def __repr__(self):
      #   return (f"(ID: {self.address_id}, "
       #         f"Street: {self.street}, City: {self.city}, Zip: {self.zip})")
-
-addr1 = Address(address_id=1,street="Hauptstrasse 5",city="Basel", zip="4051")
-addr2 = Address(address_id=2,street="Allmendweg 69",city="Sissach", zip="4450")
-addr3 = Address(address_id=3,street="Oberer Chriesmattweg 5",city="Böckten",zip="4461")
-
-print(addr1)
-print(addr2)
-print(addr3)
--- model/hotel.py
+++ model/hotel.py
@@ -1,25 +1,57 @@
-from model.address import Address
-from model.room import Room
+from model.address import Address, addr1
 from datetime import datetime
 
+
 class Hotel:
-    def __init__(self, hotel_id:int, name:str, stars:int, address:Address):
-        self.hotel_id = hotel_id
-        self.name = name
-        self.stars = stars
-        self.address = address
-        self.rooms: list[Room] = []
+    def __init__(self, hotel_id: int, name: str, stars: int, address: Address):
+        if not hotel_id:
+            raise ValueError("Address ID is required")
+        if not name:
+            raise ValueError("Street is required")
+        if not stars:
+            raise ValueError("City is required")
+        if not address:
+            raise ValueError("Zip is required")
 
+        self.__hotel_id = hotel_id
+        self.__name = name
+        self.__stars = stars
+        self.__address = address
+        self.__rooms: list = []
+
+    @property
+    def hotel_id(self):
+        return self.__hotel_id
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def stars(self):
+        return self.__stars
+    @property
+    def address(self):
+        return self.__address
+    @property
+    def rooms(self):
+        return self.__rooms
+
+    def __repr__(self):
+        return f"Hotel ID: {self.hotel_id}, Name: {self.name}, Stars: {self.stars}, Address: {self.address} "
+
+#Räume hinzufügen
     def add_room(self, room):
-        self.rooms.append(room)
+        self.__rooms.append(room)
 
+#Räume löschen
     def delete_room(self, room):
-        pass
-    # deletion process needs to be added
+        if room in self.__rooms:
+            self.__rooms.remove(room)
 
-    def get_available_rooms(self, start_date: datetime, end_date: datetime) -> list['Room']:
-        return [room for room in self.rooms if room.is_available(start_date, end_date)]
-
+#Verfügbare Räume anzeigen lassen
+    def get_available_rooms(self, start_date:datetime, end_date:datetime):
+        return  [room for room in self.__rooms if room.is_available(start_date, end_date)
+                 ]
+#Methoden von Thomas:
     def get_rooms_by_stars(self, min_stars: int, max_stars: int) -> list['Room']:
         if min_stars <= self.stars <= max_stars:
             return self.rooms
@@ -28,3 +60,16 @@ class Hotel:
 
     def get_hotel_list(self):
         return self.name, self.stars, self.address, self.rooms
+
+
+hotel = Hotel(hotel_id= 1,name= "Hotel Sunshine",stars= 4, address= addr1)
+print(hotel)
+
+
+
+hotel.add_room("Room 101")
+hotel.add_room("Room 102")
+
+print(hotel.rooms)
+
+
