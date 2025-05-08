30fd5318b589ec8c1e880977eba70ae1ef774fd4
Silian Gyger <silian.gyger@students.fhnw.ch>
2025-05-08 19:04:59 +0200

    verviundung zur Datenbank erstellt und erste Abfragen in Hotels und Adress erstellt

---
+++ data_access/address_data_access.py
@@ -0,0 +1,12 @@
+from data_access.base_data_access import BaseDataAccess
+
+class AddressDataAccess(BaseDataAccess):
+    def get_all_address(self) -> list:
+        sql = "SELECT address_id, street, city, zip_code FROM Address"
+        return self.fetchall(sql)
+
+address_dao = AddressDataAccess()
+
+address = address_dao.get_all_address()
+for address in address:
+    print(address)
