5916196e0026df10cd9e3aec0d27d6571630a869
Thomas <thomas.bartels@students.fhnw.ch>
2025-05-21 14:07:17 +0200

    2 Methoden in booking_data_access ausgeblednet

--- data_access/booking_data_access.py
+++ data_access/booking_data_access.py
@@ -17,24 +17,26 @@ class BookingDataAccess(BaseDataAccess):
 
             return [model.Booking(booking_id=booking_id) for booking_id in Booking]
 
-    def get_booking_by_id(self, booking_id: int):
-        sql = """
-            SELECT b.booking_id, b.check_in_date, b.check_out_date, b.total_amount, g.first_name, g.last_name, r.room_number, r.price_per_night
-            FROM Booking b
-            JOIN Guest g ON b.guest_id = g.guest_id
-            JOIN Room r ON b.room_id = r.room_id
-            WHERE b.booking_id = ?
-            """
-        return self.fetchone(sql, (booking_id,)) #return tuple of booking IDs
+# Delete this methode - this will be done on the booking_manager
+ #   def get_booking_by_id(self, booking_id: int):
+        #sql = """
+         #   SELECT b.booking_id, b.check_in_date, b.check_out_date, b.total_amount, g.first_name, g.last_name, r.room_number, r.price_per_night
+          #  FROM Booking b
+           # JOIN Guest g ON b.guest_id = g.guest_id
+            #JOIN Room r ON b.room_id = r.room_id
+            #WHERE b.booking_id = ?
+           # """
+        #return self.fetchone(sql, (booking_id,)) #return tuple of booking IDs
 
-    def get_booking_by_guest_id(self, guest_id: int):
-        sql = """
-            SELECT b.booking_id, b.check_in_date, b.check_out_date, b.total_amount, r.room_number, r.price_per_night
-            FROM Booking b
-            JOIN ROOM r ON b.room_id = r.room_id
-            WHERE b.guest_id = ?
-            """
-        return self.fetchone(sql, (guest_id,)) #return tuple of guest IDs
+#Delete this methode - this will be done on the booking_manager
+ #   def get_booking_by_guest_id(self, guest_id: int):
+  #      sql = """
+   #         SELECT b.booking_id, b.check_in_date, b.check_out_date, b.total_amount, r.room_number, r.price_per_night
+    #        FROM Booking b
+     #       JOIN ROOM r ON b.room_id = r.room_id
+      #      WHERE b.guest_id = ?
+       #     """
+        #return self.fetchone(sql, (guest_id,)) #return tuple of guest IDs
 
     def create_booking(self, check_in_date: str, check_out_date: str, guest_id: int, room_id: int, total_amount: float):
         sql = """
