19e131be4c886ac701f3d62e8a838aa37d7f7f18
michele <michele.lepori@bluewin.ch>
2025-05-28 11:20:46 +0200

    Address Data Access und Address Manager erstellt

--- business_logic/address_manager.py
+++ business_logic/address_manager.py
@@ -8,13 +8,35 @@ class AddressManager:
     def __init__(self, address_dal: AddressDataAccess):
         self.address_dal = address_dal
 
-    def find_address_by_id(self, address_id: int) -> Address:
-        addresses = self.address_dal.read_all_addresses()
-        for address in addresses:
-            if address.address_id == address_id:
-                return address
-        return None
+    def get_all_addresses(self) -> Address:
+        return self.address_dal.read_all_addresses()
 
+    def find_address_by_id(self, address_id: int) -> Address:
+        return self.address_dal.read_address_by_id(address_id)
+
+    def create_address(self, address: Address) -> Address:
+        new_id = self.address_dal.create_address(address)
+        return self.address_dal.read_address_by_id(new_id)
+
+    def update_address(self, address: Address) -> Address:
+        existing = self.address_dal.read_address_by_id(address.address_id)
+        if not existing:
+            return None
+        self.address_dal.update_address(address)
+        return self.address_dal.read_address_by_id(address.address_id)
+
+    def delete_address(self, address_id: int) -> bool:
+        existing = self.address_dal.read_address_by_id(address_id)
+        if not existing:
+            return False
+        self.address_dal.delete_address(address_id)
+        return True
+
+    def find_addresses_by_city(self, city: str) -> List[Address]:
+        return self.address_dal.find_addresses_by_city(city)
+
+    def find_address_by_zip(self, zip_code: str) -> List[Address]:
+        return self.address_dal.find_addresses_by_zip(zip_code)
 
 # def create_address(address_id: int, street: str, city: str, zip_code: str, country: str) -> Address:
 #     return Address(
@@ -25,8 +47,7 @@ class AddressManager:
 #         country=country
 #     )
 #
-# def list_all_addresses() -> list[Address]:
-#     return get_all_addresses()
+
 #
 # def print_all_addresses():
 #     addresses = list_all_addresses()
--- data_access/address_data_access.py
+++ data_access/address_data_access.py
@@ -22,7 +22,78 @@ class AddressDataAccess(BaseDataAccess):
             for row in Addresses
         ]
 
-# if __name__ == "__main__":
+    def read_address_by_id(self, address_id: int) -> Address | None:
+        sql = """
+        SELECT address_id, street, city, zip_code
+        FROM Address
+        WHERE address_id = ?
+        """
+        row = self.fetchone(sql, (address_id,))
+        if row:
+            return Address(
+                address_id=row[0],
+                street=row[1],
+                city=row[2],
+                zip_code=row[3]
+            )
+        return None
+
+
+    def create_address(self, address: Address) -> int:
+        sql = """
+        INSERT INTO Address (street, city, zip_code)
+        VALUES (?, ?, ?)
+        """
+        cursor = self.execute(sql, (address.street, address.city, address.zip_code))
+        return cursor.lastrowid
+
+    def update_address(self, address: Address) -> None:
+        sql = """
+        UPDATE Address
+        SET street   = ?
+            city     = ?
+            zip_code = ?
+        WHERE address_id = ?
+        """
+        self.execute(sql, (address.street, address.city, address.zip_code, address.address_id))
+
+    def delete_address(self, address_id: int) -> None:
+        sql = """
+        DELETE FROM Address
+        WHERE address_id = ?
+        """
+        self.execute(sql, (address_id,))
+
+    def find_addresses_by_city(self, city: str) -> list[Address]:
+        sql = """
+        SELECT address_id, street, city, zip_code
+        FROM Address
+        WHERE city = ?
+        """
+        rows = self.fetchall(sql, (city,))
+        return [Address(address_id=row[0],
+                        street=row[1],
+                        city=row[2],
+                        zip_code=row[3]
+                        )
+                for row in rows
+                ]
+
+    def find_addresses_by_zip(self, zip_code: str) -> list[Address]:
+        sql = """
+              SELECT address_id, street, city, zip_code
+              FROM Address
+              WHERE zip_code = ?
+              """
+        rows = self.fetchall(sql, (zip_code,))
+        return [Address(address_id=row[0],
+                        street=row[1],
+                        city=row[2],
+                        zip_code=row[3]
+                        )
+                for row in rows]
+
+## if __name__ == "__main__":
 #     db_path = "../database/hotel_sample.db"
 #     address_dal = AddressDataAccess(db_path)
 #     addresses = address_dal.read_all_addresses()
