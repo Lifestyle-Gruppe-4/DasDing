9de42ba41979fe0dc0d99834db43d4283dd26de0
Thomas <thomas.bartels2@gmail.com>
2025-06-15 09:43:17 +0200

    corrected variable into lowercase

--- data_access/address_data_access.py
+++ data_access/address_data_access.py
@@ -10,7 +10,7 @@ class AddressDataAccess(BaseDataAccess):
         SELECT address_id, street, city, zip_code
         FROM Address
         """
-        Addresses = self.fetchall(sql)
+        addresses = self.fetchall(sql)
 
         return [
             Address(
@@ -19,7 +19,7 @@ class AddressDataAccess(BaseDataAccess):
                 city=row[2],
                 zip_code=row[3]
             )
-            for row in Addresses
+            for row in addresses
         ]
 
     def read_address_by_id(self, address_id: int) -> Address | None:
@@ -100,29 +100,3 @@ class AddressDataAccess(BaseDataAccess):
                         zip_code=row[3]
                         )
                 for row in rows]
-
-# if __name__ == "__main__":
-#     db_path = "../database/hotel_sample.db"
-#     address_dal = AddressDataAccess(db_path)
-#     addresses = address_dal.read_all_addresses()
-#     for address in addresses:
-#         print(address)
-
-
-# def get_all_addresses() -> list[Address]:
-#     base = BaseDataAccess("database/hotel_sample.db")
-#     sql = "SELECT address_id, street, city, zip_code, country FROM address"
-#     rows = base.fetchall(sql)
-#
-#     addresses = []
-#     for row in rows:
-#         address = Address(
-#             address_id=row[0],
-#             street=row[1],
-#             city=row[2],
-#             zip_code=row[3],
-#             country=row[4]
-#         )
-#         addresses.append(address)
-#
-#     return addresses
