32093b245ed47ab5f03d7cc6f01be8ab1b950288
Thomas <thomas.bartels2@gmail.com>
2025-06-15 09:39:31 +0200

    deleted unused code

--- model/address.py
+++ model/address.py
@@ -35,25 +35,3 @@ class Address:
 
     def __repr__(self):
         return f"Address ID: {self.address_id}, Street: {self.street}, City: {self.city}, ZIP: {self.zip_code}"
-
-
-# addr1 = Address(address_id=1,street="Hauptstrasse 5",city="Basel", zip_code="4051")
-# addr2 = Address(address_id=2,street="Allendweg 69",city="Sissach", zip="4450")
-# addr3 = Address(address_id=3,street="Oberer Chriesmattweg 5",city="Böckten",zip="4461")
-#
-# print(addr1)
-# print(addr2)
-# print(addr3)
-
-
-#Address Class defined by Silian in a first step
-#class Address:
-#    def __init__(self, address_id:int,street:str,city:str, zip:str):
- #       self.address_id = address_id
-  #      self.street = street
-   #     self.city = city
-    #    self.zip = zip
-
-    #def __repr__(self):
-     #   return (f"(ID: {self.address_id}, "
-      #         f"Street: {self.street}, City: {self.city}, Zip: {self.zip})")
